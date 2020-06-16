bd563b6edf004cd2ec8acc95ccdf5d71e268c9aa
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-16 16:44:48 +0300

    Fix some form-relates test errors

--- src/forms/uploadXMLForm.test.js
+++ src/forms/uploadXMLForm.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
 //import { render, fireEvent, wait } from "@testing-library/react"
-import { fireEvent, wait } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import configureStore from "redux-mock-store"
 import { Provider } from "react-redux"
 

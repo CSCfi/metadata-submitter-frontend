470596d611a2eeea8f8959985c256b0c039d3a07
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-06-30 07:49:27 +0300

    Fix development environment related proxy bugs

---
+++ src/setupProxy.js
@@ -0,0 +1,17 @@
+/* eslint no-undef: 0 */
+/* Setup proxy for development usage. Disable eslint rule, since react handles
+ * importing logic on the background. */
+
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+const proxy = process.env.REACT_APP_BACKEND_PROXY || "localhost:5430"
+
+module.exports = function (app) {
+  app.use(
+    ["/objects", "/schemas", "/validate", "/submit"],
+    createProxyMiddleware({
+      target: `http://${proxy}`,
+      changeOrigin: true,
+    })
+  )
+}

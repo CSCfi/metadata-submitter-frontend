bf2b3e0cd2061248243f111e3ddb1888ddcd9d0b
Stefan Negru <stefan@blankdots.com>
2020-08-11 16:49:46 +0300

    base for login menu

---
+++ src/components/login.js
@@ -0,0 +1,20 @@
+//@flow
+import React from "react"
+import Grid from "@material-ui/core/Grid"
+// import Card from "@material-ui/core/Card"
+
+// import { makeStyles } from "@material-ui/core/styles"
+
+// const useStyles = makeStyles(theme => ({
+//     cardGrid: {
+//         paddingTop: theme.spacing(8),
+//         paddingBottom: theme.spacing(8),
+//     },
+// }))
+
+const Login = () => {
+  //   const classes = useStyles()
+  return <Grid container direction="column" justify="space-between" alignItems="stretch"></Grid>
+}
+
+export default Login

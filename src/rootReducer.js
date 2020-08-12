5a48c08ef967f3d068413bd94af1dd58817b9551
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-08-12 21:05:32 +0300

    Add reducer for submissionType

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,6 +2,7 @@
 
 import { combineReducers } from "@reduxjs/toolkit"
 
+import submissionTypeReducer from "features/submissionTypeSlice"
 import objectTypeReducer from "features/objectTypeSlice"
 import wizardStepReducer from "features/wizardStepSlice"
 import submissionFolderReducer from "features/submissionFolderSlice"
@@ -10,6 +11,7 @@ const rootReducer = combineReducers({
   objectType: objectTypeReducer,
   wizardStep: wizardStepReducer,
   submissionFolder: submissionFolderReducer,
+  submissionType: submissionTypeReducer
 })
 
 export default rootReducer

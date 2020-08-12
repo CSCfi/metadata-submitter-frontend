0067b7b10c535e9ed0c44c71713de172a2e38c80
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-08-12 19:10:59 +0300

    Add better spacing for summary fields

--- src/components/NewDraftWizard/WizardSteps/WizardShowSummaryStep.js
+++ src/components/NewDraftWizard/WizardSteps/WizardShowSummaryStep.js
@@ -13,7 +13,7 @@ import ListItemText from "@material-ui/core/ListItemText"
 const useStyles = makeStyles(theme => ({
   summary: {
     padding: theme.spacing(1),
-    width: "30vw",
+    width: theme.spacing(100),
   },
   schemaTitleRow: {
     display: "flex",

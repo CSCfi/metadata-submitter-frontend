1aa97e522c00b058d9e19572293be81a73033e6c
Otto Ahoniemi <otto.ahoniemi@csc.fi>
2020-08-13 17:21:02 +0300

    Add info comments for proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,8 @@
-/* eslint no-undef: 0 */
-/* Setup proxy for development usage. Disable eslint rule, since react handles
- * importing logic on the background. */
-
+/*
+Proxy for development usage. All the paths that should be forwarded to backend should be added here.
+React handles proxy logic in the background, see https://create-react-app.dev/docs/proxying-api-requests-in-development/
+*/
 const { createProxyMiddleware } = require("http-proxy-middleware")
-
 const proxy = process.env.REACT_APP_BACKEND_PROXY || "localhost:5430"
 
 module.exports = function (app) {

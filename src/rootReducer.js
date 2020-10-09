981a7729659897336edf0ea977299d461b8fee4d
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-09 14:48:23 +0300

    Added error message reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,12 +2,14 @@
 
 import { combineReducers } from "@reduxjs/toolkit"
 
+import wizardErrorMessageReducer from "features/wizardErrorMessageSlice"
 import objectTypeReducer from "features/wizardObjectTypeSlice"
 import wizardStepReducer from "features/wizardStepSlice"
 import submissionFolderReducer from "features/wizardSubmissionFolderSlice"
 import submissionTypeReducer from "features/wizardSubmissionTypeSlice"
 
 const rootReducer = combineReducers({
+  errorMessage: wizardErrorMessageReducer,
   objectType: objectTypeReducer,
   wizardStep: wizardStepReducer,
   submissionFolder: submissionFolderReducer,

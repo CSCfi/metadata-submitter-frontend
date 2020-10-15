b5186a25a3a858cb52a5b590df8f33af880bf946
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-15 08:44:37 +0300

    Migrated from Enzyme to React testing library

--- src/__tests__/WizardStatusMessageHandler.test.js
+++ src/__tests__/WizardStatusMessageHandler.test.js
@@ -1,10 +1,10 @@
 import React from "react"
 
+import { render, screen } from "@testing-library/react"
 import { Provider } from "react-redux"
 import configureStore from "redux-mock-store"
 
 import WizardStatusMessageHandler from "../components/NewDraftWizard/WizardForms/WizardStatusMessageHandler"
-import { shallow, mount } from "../enzyme"
 
 const mockStore = configureStore([])
 
@@ -12,20 +12,37 @@ describe("WizardStatusMessageHandler", () => {
   const store = mockStore({
     errorMessage: "",
   })
-  it("should render appropriate components", () => {
-    const statusList = ["error", "info", "success"]
-    statusList.forEach(status => {
-      const wrapper = shallow(<WizardStatusMessageHandler successStatus={status} />)
-      expect(wrapper.find(status.charAt(0).toUpperCase() + status.slice(1) + "Handler").length).toBe(1)
-    })
-  })
 
-  it("should open snackbar on error message", () => {
-    const wrapper = mount(
+  it("should render error message", () => {
+    const responseMock = { data: { accessionId: "TESTID1234", status: 504 } }
+    const prefixTextMock = "Test prefix"
+    render(
       <Provider store={store}>
-        <WizardStatusMessageHandler response={{ status: 504 }} successStatus="error" />
+        <WizardStatusMessageHandler successStatus={"error"} response={responseMock} prefixText={prefixTextMock} />
       </Provider>
     )
-    expect(wrapper.find("ForwardRef(Snackbar)").length).toBe(1)
+    expect(screen.getByRole("alert")).toBeDefined()
+    expect(screen.getByText(/Unfortunately an unexpected error happened on our servers/i)).toBeDefined()
+  })
+
+  it("should render info message", () => {
+    render(
+      <Provider store={store}>
+        <WizardStatusMessageHandler successStatus={"info"} />
+      </Provider>
+    )
+    expect(
+      screen.getByText(/For some reason, your file is still being saved to our database, please wait./i)
+    ).toBeDefined()
+  })
+
+  it("should render success message", () => {
+    const responseMock = { data: { accessionId: "TESTID1234" } }
+    render(
+      <Provider store={store}>
+        <WizardStatusMessageHandler response={responseMock} successStatus={"success"} />
+      </Provider>
+    )
+    expect(screen.getByText(/Submitted with accessionid TESTID1234/i)).toBeDefined()
   })
 })

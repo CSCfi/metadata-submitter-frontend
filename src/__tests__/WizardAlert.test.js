9e226b7126b0aa0557b494b54f618a4b979ce1ea
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-15 09:43:23 +0300

    Migrated from Enzyme to RTL

--- src/__tests__/WizardAlert.test.js
+++ src/__tests__/WizardAlert.test.js
@@ -1,18 +1,19 @@
 import React from "react"
 
+import { render, screen } from "@testing-library/react"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import configureStore from "redux-mock-store"
 
 import WizardAlert from "../components/NewDraftWizard/WizardComponents/WizardAlert"
-import { mount } from "../enzyme"
 
 const mockStore = configureStore([])
 
 describe("WizardAlert", () => {
   const store = mockStore({
-    subMissionType: "",
+    submissionType: "",
   })
+
   it("should render appropriate dialogs", () => {
     const alerts = [
       { submission: { types: ["form", "xml", "existing"] } },
@@ -21,14 +22,14 @@ describe("WizardAlert", () => {
     ]
     alerts.forEach(item => {
       item[Object.keys(item)].types.forEach(type => {
-        const wrapper = mount(
+        render(
           <BrowserRouter>
             <Provider store={store}>
               <WizardAlert alertType={type} parentLocation={Object.keys(item)[0]} onAlert="true" />
             </Provider>
           </BrowserRouter>
         )
-        expect(wrapper.find("ForwardRef(Dialog)").length).toBe(1)
+        expect(screen.getByRole("dialog")).toBeDefined()
       })
     })
   })

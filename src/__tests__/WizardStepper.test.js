e75ac883fa87553e3760d80f21b1e54edc51f562
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-16 11:25:04 +0300

    Removed snapshot test and snapshot file

--- src/__tests__/WizardStepper.test.js
+++ src/__tests__/WizardStepper.test.js
@@ -1,43 +1,26 @@
 import React from "react"
 
 import "@testing-library/jest-dom/extend-expect"
-import { render, screen, fireEvent } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Provider } from "react-redux"
 import configureStore from "redux-mock-store"
-import { toMatchDiffSnapshot } from "snapshot-diff"
 
 import WizardStepper from "../components/NewDraftWizard/WizardComponents/WizardStepper"
 
 const mockStore = configureStore([])
 
-expect.extend({ toMatchDiffSnapshot })
-
 describe("WizardStepper", () => {
-  let store
-  let wrapper
-
-  beforeEach(() => {
-    store = mockStore({
-      submissionType: "form",
-      wizardStep: 1,
+  it("should have back-step button disabled on first step", () => {
+    const store = mockStore({
+      submissionType: "",
+      wizardStep: 0,
     })
-    wrapper = (
+    render(
       <Provider store={store}>
         <WizardStepper />
       </Provider>
     )
-  })
-
-  it("should take a snapshot", () => {
-    const { asFragment } = render(wrapper)
-    expect(asFragment(wrapper)).toMatchSnapshot()
-  })
-
-  it("should have back-step button disabled when navigated to first step", () => {
-    const { asFragment } = render(wrapper)
-    const firstRender = asFragment()
     const button = screen.getByRole("button", { name: /Back/i })
-    fireEvent.click(button)
-    expect(firstRender).toMatchDiffSnapshot(asFragment())
+    expect(button).toHaveAttribute("disabled")
   })
 })

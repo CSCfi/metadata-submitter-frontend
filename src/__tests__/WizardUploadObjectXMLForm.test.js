d97bc2cd380a5f43edce09998486d276a7f4267a
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-16 11:45:11 +0300

    Test that upload for file works

--- src/__tests__/WizardUploadObjectXMLForm.test.js
+++ src/__tests__/WizardUploadObjectXMLForm.test.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import "@testing-library/jest-dom/extend-expect"
 import { render, screen } from "@testing-library/react"
-// import userEvent from "@testing-library/user-event"
+import userEvent from "@testing-library/user-event"
 import { Provider } from "react-redux"
 import configureStore from "redux-mock-store"
 import { toMatchDiffSnapshot } from "snapshot-diff"
@@ -33,22 +33,17 @@ describe("WizardStepper", () => {
     expect(screen.getByRole("button", { name: /save/i })).toHaveAttribute("disabled")
   })
 
-  // it("should enable save button when there's validated xml file", async () => {
-  //   // const file = new File(["test"], "test.xml", { type: "xml" })
-  //   render(
-  //     <Provider store={store}>
-  //       <WizardUploadObjectXMLForm />
-  //     </Provider>
-  //   )
-  //   // const input = screen.getByRole("textbox")
-  //   // userEvent.upload(input, file)
-  //   // fireEvent.change(input, {
-  //   //   target: {
-  //   //     value: [file],
-  //   //   },
-  //   // })
-  //   // expect(screen.getByRole("button", { name: /save/i })).toHaveAttribute("disabled")
-  //   expect(screen.getByText(/Please attach an XML file./i)).toBeDefined()
-  //   screen.debug()
-  // })
+  it("should have uploaded file in input", async () => {
+    const file = new File(["test"], "test.xml", { type: "text/xml" })
+    render(
+      <Provider store={store}>
+        <WizardUploadObjectXMLForm />
+      </Provider>
+    )
+    const input = screen.getByRole("textbox")
+    userEvent.upload(input, file)
+
+    expect(input.files[0]).toStrictEqual(file)
+    expect(input.files).toHaveLength(1)
+  })
 })

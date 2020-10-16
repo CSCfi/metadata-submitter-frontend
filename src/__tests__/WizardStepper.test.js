ac4a92c90c9baedab2ebd2c4fd9e263d7f00e76a
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-16 11:40:43 +0300

    Test for dialog

--- src/__tests__/WizardStepper.test.js
+++ src/__tests__/WizardStepper.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import "@testing-library/jest-dom/extend-expect"
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import { Provider } from "react-redux"
 import configureStore from "redux-mock-store"
 
@@ -22,5 +22,19 @@ describe("WizardStepper", () => {
     )
     const button = screen.getByRole("button", { name: /Back/i })
     expect(button).toHaveAttribute("disabled")
+  })
+  it("should open dialog if form or upload in progress", () => {
+    const store = mockStore({
+      submissionType: "form",
+      wizardStep: 1,
+    })
+    render(
+      <Provider store={store}>
+        <WizardStepper />
+      </Provider>
+    )
+    const button = screen.getByRole("button", { name: /Back/i })
+    fireEvent.click(button)
+    expect(screen.getByRole("dialog")).toBeDefined
   })
 })

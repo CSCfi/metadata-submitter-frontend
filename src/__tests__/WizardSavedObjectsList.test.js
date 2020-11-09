f4d92ba62f062e1008ca6028a7556d11b2b79367
saulipurhonen <sauli.purhonen@gmail.com>
2020-11-09 12:59:19 +0200

    Basic test form list

---
+++ src/__tests__/WizardSavedObjectsList.test.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+import "@testing-library/jest-dom/extend-expect"
+import { render, screen } from "@testing-library/react"
+import { Provider } from "react-redux"
+import configureStore from "redux-mock-store"
+
+import WizardSavedObjectsList from "../components/NewDraftWizard/WizardComponents/WizardSavedObjectsList"
+
+const mockStore = configureStore([])
+
+describe("WizardStepper", () => {
+  const store = mockStore({
+    submissionType: "sample",
+    wizardStep: 1,
+  })
+
+  const submissions = [
+    { accessionId: "EDAG1", schema: "sample" },
+    { accessionId: "EDAG2", schema: "sample", new: true },
+  ]
+
+  it("should have 'Added!' message rendered on item that has 'new' property", () => {
+    render(
+      <Provider store={store}>
+        <WizardSavedObjectsList submissions={submissions} submissionType="sample" />
+      </Provider>
+    )
+    expect(screen.getByText("Added!")).toBeInTheDocument()
+  })
+})

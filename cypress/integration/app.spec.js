843f04a1ef98bc3f8aab8aea9d0ec1ff7054adb4
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-04 10:44:03 +0200

    Test for XML file upload

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -22,8 +22,21 @@ describe("Basic e2e", function () {
     cy.get("select[name='descriptor.studyType']").select("Metagenomics")
     cy.get("button[type=submit]").contains("Save").click()
 
+    // Upload an xml file. Need to wait for save. TODO: Better logic for waiting until last save is done
+    cy.wait(2000).get("div[role=button]").contains("Upload XML File").click()
+    cy.fixture("study_test.xml").then(fileContent => {
+      cy.get('input[type="file"]').attachFile({
+        fileContent: fileContent.toString(),
+        fileName: "testFile.xml",
+        mimeType: "text/xml",
+        force: true,
+      })
+    })
+    // Hacky way to get past RHF watch -method problem that doesn't allow cypress to get updated value for file
+    cy.get("form").submit()
+
     // Saved objects list should have newly added item
-    cy.get(".MuiListItem-container", { timeout: 10000 }).should("have.length", 1)
+    cy.get(".MuiListItem-container", { timeout: 10000 }).should("have.length", 2)
 
     // // Navigate to summary and publish
     cy.get("button[type=button]").contains("Next").click()

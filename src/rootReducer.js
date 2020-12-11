738b893dad38209afb8a01bb36f7fbfa93acbc7e
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-11 10:07:20 +0200

    Reducer for status details

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -4,14 +4,14 @@ import { combineReducers } from "@reduxjs/toolkit"
 
 import draftStatusReducer from "features/draftStatusSlice"
 import draftObjectReducer from "features/wizardDraftObjectSlice"
-import wizardErrorMessageReducer from "features/wizardErrorMessageSlice"
 import objectTypeReducer from "features/wizardObjectTypeSlice"
+import wizardStatusMessageReducer from "features/wizardStatusMessageSlice"
 import wizardStepReducer from "features/wizardStepSlice"
 import submissionFolderReducer from "features/wizardSubmissionFolderSlice"
 import submissionTypeReducer from "features/wizardSubmissionTypeSlice"
 
 const rootReducer = combineReducers({
-  errorMessage: wizardErrorMessageReducer,
+  statusDetails: wizardStatusMessageReducer,
   objectType: objectTypeReducer,
   wizardStep: wizardStepReducer,
   submissionFolder: submissionFolderReducer,

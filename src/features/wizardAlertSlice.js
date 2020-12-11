b14caf45556a06a3151ed47d8343b72d7cab5645
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-11 14:41:10 +0200

    Slice for alert dialog status

---
+++ src/features/wizardAlertSlice.js
@@ -0,0 +1,16 @@
+//@flow
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = false
+
+const wizardAlertSlice = createSlice({
+  name: "draftStatus",
+  initialState,
+  reducers: {
+    setAlert: () => true,
+    resetAlert: () => initialState,
+  },
+})
+
+export const { setAlert, resetAlert } = wizardAlertSlice.actions
+export default wizardAlertSlice.reducer

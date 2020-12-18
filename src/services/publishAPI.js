2f0438c01d5a1ed261d8cdd43d41ee54ad80e155
HangLe <hannyle91@gmail.com>
2020-12-18 18:15:56 +0200

    Add apisauce monitor to check api requests and catch errors

--- src/services/publishAPI.js
+++ src/services/publishAPI.js
@@ -1,7 +1,10 @@
 //@flow
 import { create } from "apisauce"
 
+import { errorMonitor } from "./errorMonitor"
+
 const api = create({ baseURL: "/publish" })
+api.addMonitor(errorMonitor)
 
 const publishFolderById = async (folderId: string) => {
   return await api.patch(`/${folderId}`)
--- src/services/schemaAPI.js
+++ src/services/schemaAPI.js
@@ -1,7 +1,10 @@
 //@flow
 import { create } from "apisauce"
 
+import { errorMonitor } from "./errorMonitor"
+
 const api = create({ baseURL: "/schemas" })
+api.addMonitor(errorMonitor)
 
 const getSchemaByObjectType = async (objectType: string) => {
   return await api.get(`/${objectType}`)

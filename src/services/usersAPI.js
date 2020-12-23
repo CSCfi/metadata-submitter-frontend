41ee27d7ce435b0c6c60fdc31bf19c0aed42d8d2
Stefan Negru <stefan@blankdots.com>
2020-12-23 14:18:26 +0200

    no need for apicall logout

--- src/services/usersAPI.js
+++ src/services/usersAPI.js
@@ -11,13 +11,7 @@ const deleteUserID = async (userID: string) => {
   return await api.delete(`/${userID}`)
 }
 
-const logoutUser = async () => {
-  const logout = create({ baseURL: "/logout" })
-  return await logout.get()
-}
-
 export default {
   getUserID,
   deleteUserID,
-  logoutUser,
 }

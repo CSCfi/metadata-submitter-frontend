5a3ce333c19952d170e8e56865208f64ddd4b778
HangLe <hannyle91@gmail.com>
2021-02-03 16:48:17 +0200

    Add new form buttons on top of form

--- src/components/NewDraftWizard/WizardComponents/WizardAddObjectCard.js
+++ src/components/NewDraftWizard/WizardComponents/WizardAddObjectCard.js
@@ -1,33 +1,20 @@
 //@flow
 import React from "react"
 
-import Button from "@material-ui/core/Button"
 import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import CardHeader from "@material-ui/core/CardHeader"
 import { makeStyles } from "@material-ui/core/styles"
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 
 import WizardDraftObjectPicker from "components/NewDraftWizard/WizardComponents/WizardDraftObjectPicker"
 import WizardFillObjectDetailsForm from "components/NewDraftWizard/WizardForms/WizardFillObjectDetailsForm"
 import WizardUploadObjectXMLForm from "components/NewDraftWizard/WizardForms/WizardUploadObjectXMLForm"
-import { resetObjectType } from "features/wizardObjectTypeSlice"
-import { resetSubmissionType } from "features/wizardSubmissionTypeSlice"
 
 const useStyles = makeStyles(theme => ({
   card: {
     width: "100%",
     marginLeft: theme.spacing(2),
     marginBottom: theme.spacing(4),
-  },
-  cardHeader: {
-    backgroundColor: theme.palette.primary.main,
-    color: "#FFF",
-    fontWeight: "bold",
-  },
-  cardHeaderAction: {
-    marginTop: "-4px",
-    marginBottom: "-4px",
+    padding: 0,
   },
   cardCenterContent: {
     flexGrow: 1,
@@ -35,6 +22,8 @@ const useStyles = makeStyles(theme => ({
     justifyContent: "center",
     alignItems: "center",
     minHeight: "40vh",
+    border: "1px solid blue",
+    padding: 0,
   },
   submissionTypeButton: {
     paddingTop: theme.spacing(2),
@@ -47,37 +36,6 @@ const useStyles = makeStyles(theme => ({
     marginTop: 0,
   },
 }))
-
-/*
- * Create header for form card with button to close the card
- */
-const CustomCardHeader = ({ title }: { title: string }) => {
-  const classes = useStyles()
-  const dispatch = useDispatch()
-  return (
-    <CardHeader
-      title={title}
-      titleTypographyProps={{ variant: "inherit" }}
-      classes={{
-        root: classes.cardHeader,
-        action: classes.cardHeaderAction,
-      }}
-      action={
-        <Button
-          variant="outlined"
-          aria-label="hide card"
-          className={classes.hideButton}
-          onClick={() => {
-            dispatch(resetObjectType())
-            dispatch(resetSubmissionType())
-          }}
-        >
-          Hide
-        </Button>
-      }
-    />
-  )
-}
 
 /*
  * Render correct form to add objects based on submission type in store
@@ -105,10 +63,7 @@ const WizardAddObjectCard = () => {
   }
   return (
     <Card className={classes.card} data-testid={cards[submissionType]["testId"]}>
-      <CustomCardHeader title={cards[submissionType]["title"]} />
-      <CardContent className={submissionType === "xml" ? classes.cardCenterContent : ""}>
-        {cards[submissionType]["component"]}
-      </CardContent>
+      {cards[submissionType]["component"]}
     </Card>
   )
 }

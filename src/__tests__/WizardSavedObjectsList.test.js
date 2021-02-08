214825ab09959750d5bfae4f50caf53e3153f7c1
HangLe <hannyle91@gmail.com>
2021-02-08 12:29:49 +0200

    Add unit test for submitted items list

--- src/__tests__/WizardSavedObjectsList.test.js
+++ src/__tests__/WizardSavedObjectsList.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import "@testing-library/jest-dom/extend-expect"
-import { render, screen } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 import { Provider } from "react-redux"
 import configureStore from "redux-mock-store"
 
@@ -11,23 +11,51 @@ const mockStore = configureStore([])
 
 describe("WizardStepper", () => {
   const store = mockStore({
-    submissionType: "sample",
+    objectType: "sample",
     wizardStep: 1,
   })
 
   const submissions = [
-    { accessionId: "EDAG1", schema: "sample" },
-    { accessionId: "EDAG2", schema: "sample" },
+    { accessionId: "EDAG1", schema: "sample", tags: { submissionType: "Form" } },
+    { accessionId: "EDAG2", schema: "sample", tags: { submissionType: "XML" } },
+    { accessionId: "EDAG3", schema: "sample", tags: { submissionType: "XML" } },
   ]
 
-  it("should have saved objects listed", () => {
+  beforeEach(() => {
     render(
       <Provider store={store}>
-        <WizardSavedObjectsList submissions={submissions} submissionType="sample" />
+        <WizardSavedObjectsList submissions={submissions} />
       </Provider>
     )
+  })
+
+  it("should have saved objects listed", () => {
     submissions.forEach(item => {
       expect(screen.getByText(item.accessionId)).toBeInTheDocument()
     })
+  })
+
+  it("should have correct amount of submitted forms", () => {
+    screen.getByText(/Submitted sample form/i)
+    expect(screen.getByText(/Submitted sample form/i)).toBeInTheDocument()
+
+    const formList = screen.getByRole("list", { name: "Form" })
+    expect(formList).toBeInTheDocument()
+
+    const { getAllByRole } = within(formList)
+    const submittedForms = getAllByRole("listitem")
+    expect(submittedForms.length).toBe(1)
+  })
+
+  it("should have correct amount of submitted forms", () => {
+    screen.getByText(/Submitted sample xml/i)
+    expect(screen.getByText(/Submitted sample xml/i)).toBeInTheDocument()
+
+    const xmlList = screen.getByRole("list", { name: "XML" })
+    expect(xmlList).toBeInTheDocument()
+
+    const { getAllByRole } = within(xmlList)
+    const submittedXML = getAllByRole("listitem")
+    expect(submittedXML.length).toBe(2)
   })
 })

4f4ec247ea966764c45c180f3b7486b653569a1c
Sauli Purhonen <sauli.purhonen@gmail.com>
2021-03-30 15:13:39 +0300

    Test blur on form field

--- src/__tests__/WizardFillObjectDetailsForm.test.js
+++ src/__tests__/WizardFillObjectDetailsForm.test.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import "@testing-library/jest-dom/extend-expect"
 import { ThemeProvider } from "@material-ui/core/styles"
-import { render, screen, waitFor } from "@testing-library/react"
+import { fireEvent, render, screen, waitFor } from "@testing-library/react"
 import { Provider } from "react-redux"
 import configureStore from "redux-mock-store"
 
@@ -27,6 +27,11 @@ describe("WizardFillObjectDetailsForm", () => {
           studyTitle: {
             title: "Study Title",
             description: "Title of the study as would be used in a publication.",
+            type: "string",
+          },
+          studyTestField: {
+            title: "Study Test Field",
+            description: "Study test field description.",
             type: "string",
           },
         },
@@ -57,6 +62,22 @@ describe("WizardFillObjectDetailsForm", () => {
     )
     await waitFor(() => screen.getByText("Study Description"))
     expect(screen.getByText("Study Description")).toBeDefined()
+  })
+
+  it("should validate without errors on blur", async () => {
+    render(
+      <Provider store={store}>
+        <ThemeProvider theme={CSCtheme}>
+          <WizardFillObjectDetailsForm />
+        </ThemeProvider>
+      </Provider>
+    )
+    await waitFor(() => {
+      const input = screen.getByTestId("descriptor.studyTitle")
+      fireEvent.change(input, { target: { value: "Title" } })
+      fireEvent.blur(input)
+      expect(input).toBeVisible()
+    })
   })
 
   // Note: If this test runs before form creation, form creation fails because getItem spy messes sessionStorage init somehow

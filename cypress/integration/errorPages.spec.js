fd7b50d812237dc8398f608e463478d988d869eb
HangLe <hannyle91@gmail.com>
2021-04-07 17:27:54 +0300

    Remove logout button for 401 test

--- cypress/integration/errorPages.spec.js
+++ cypress/integration/errorPages.spec.js
@@ -2,7 +2,6 @@ describe("catch error codes and display corresponding error page", function () {
   const baseUrl = "http://localhost:" + Cypress.env("port") + "/"
 
   it("should redirect to 401 page if no granted access", () => {
-    cy.contains("Log out").click()
     cy.visit(baseUrl + "folders")
     cy.contains(".MuiAlert-message", "401 Authorization Error")
   })

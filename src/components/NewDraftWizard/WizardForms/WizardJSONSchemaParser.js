e6f8e143e6a8a07854dcc95c72a783021ff1ee3a
Liisa Lado-Villar <47525503+lilachic@users.noreply.github.com>
2021-06-24 09:29:57 +0300

    add unit test for tootip

--- src/components/NewDraftWizard/WizardForms/WizardJSONSchemaParser.js
+++ src/components/NewDraftWizard/WizardForms/WizardJSONSchemaParser.js
@@ -36,7 +36,6 @@ const highlightStyle = theme => {
 
 const helpIconStyle = makeStyles(theme => ({
   fieldTip: {
-    bordercolor: "#FF00000",
     color: theme.palette.secondary.main,
     marginLeft: theme.spacing(0),
   }

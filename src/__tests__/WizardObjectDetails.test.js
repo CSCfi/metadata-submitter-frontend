997fcfc9de97e22dfee8a8d6627675c693bf3926
Sauli Purhonen <sauli.purhonen@visma.com>
2021-09-10 14:34:17 +0300

    Object details integration test

--- src/__tests__/WizardObjectDetails.test.js
+++ src/__tests__/WizardObjectDetails.test.js
@@ -1,13 +1,18 @@
 import React from "react"
 
 import "@testing-library/jest-dom/extend-expect"
-
+import { ThemeProvider } from "@material-ui/core/styles"
 import { render, screen, waitForElementToBeRemoved } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { rest } from "msw"
 import { setupServer } from "msw/node"
 import { Provider } from "react-redux"
+import { MemoryRouter } from "react-router-dom"
 import configureStore from "redux-mock-store"
 
+import CSCtheme from "../theme"
+
+import SelectedFolderDetails from "components/Home/SelectedFolderDetails"
 import WizardObjectDetails from "components/NewDraftWizard/WizardComponents/WizardObjectDetails"
 
 const mockStore = configureStore([])
@@ -108,6 +113,61 @@ describe("Object details", () => {
   beforeAll(() => server.listen())
   afterEach(() => server.resetHandlers())
   afterAll(() => server.close())
+
+  it("should render object details component on 'Show details' button click", async () => {
+    server.use(
+      rest.get("/schemas/:schema", (req, res, ctx) => {
+        return res(ctx.json(schema))
+      }),
+      rest.get("/folders/:folderId", (req, res, ctx) => {
+        return res(
+          ctx.json({
+            name: "Test Folder",
+            description: "Folder description",
+            published: true,
+            metadataObjects: [
+              {
+                accessionId: "objectAccessionId",
+                schema: "study",
+                tags: { submissionType: "Form", displayTitle: "Published object" },
+              },
+            ],
+            drafts: [],
+            folderId: "testaccessionid",
+            dateCreated: 1111111111,
+          })
+        )
+      }),
+      rest.get("/objects/study/:accessionId", (req, res, ctx) => {
+        return res(
+          ctx.json({
+            descriptor: { studyTitle: "Published object", studyType: "Synthetic Genomics" },
+            accessionId: "edd227d34a4f4340a206ef48c87848da",
+            dateCreated: "2021-09-09T05:16:36.002000",
+            dateModified: "2021-09-09T05:16:36.002000",
+            publishDate: "2021-11-09T05:16:36.002000",
+          })
+        )
+      })
+    )
+
+    render(
+      <MemoryRouter initialEntries={[{ pathname: "/home/published/testaccessionid" }]}>
+        <Provider store={store}>
+          <ThemeProvider theme={CSCtheme}>
+            <SelectedFolderDetails />
+          </ThemeProvider>
+        </Provider>
+      </MemoryRouter>
+    )
+
+    await waitForElementToBeRemoved(() => screen.getByRole("progressbar"))
+    userEvent.click(screen.getByTestId("toggle-details"))
+
+    await waitForElementToBeRemoved(() => screen.getByRole("progressbar"))
+    expect(screen.getByText("Hide details")).toBeInTheDocument()
+    expect(screen.getByTestId("section")).toBeInTheDocument()
+  })
 
   it("should render object details by object data and schema", async () => {
     server.use(

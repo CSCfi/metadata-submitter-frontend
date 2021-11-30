8a0e3aa170275c364e429b73b35960eb5e9e9d48
Stefan Negru <stefan@blankdots.com>
2021-11-30 11:59:56 +0200

    typo fixes

--- src/services/errorMonitor.js
+++ src/services/errorMonitor.js
@@ -1,6 +1,6 @@
 // function to catch errors
 export const errorMonitor = res => {
-  // Exceptional case: replacing XML file with same name oftenly causes error. We don't redirect to 400 page but only show error message.
+  // Exceptional case: replacing XML file with same name often causes error. We don't redirect to 400 page but only show error message.
   const exceptionalCase = res.config.baseURL === "/objects" && res.config.method === "put"
   if (!res.ok && !exceptionalCase) {
     switch (res.status) {

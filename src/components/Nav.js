ab190481e04457660040ce6d590a826cd47d507c
HangLe <hannyle91@gmail.com>
2021-12-09 17:49:00 +0200

    Update for Login page's Navigation bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,4 @@
+//@flow
 import React, { useState } from "react"
 
 import HomeIcon from "@mui/icons-material/Home"
@@ -7,10 +8,12 @@ import IconButton from "@mui/material/IconButton"
 import Link from "@mui/material/Link"
 import Menu from "@mui/material/Menu"
 import MenuItem from "@mui/material/MenuItem"
+import { styled } from "@mui/material/styles"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import { makeStyles } from "@mui/styles"
-import * as i18n from "i18next"
+import i18n from "i18next"
+import { useDispatch, useSelector } from "react-redux"
 import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom"
 
 import logo from "../images/csc_logo.svg"
@@ -20,26 +23,12 @@ import { setLocale } from "features/localeSlice"
 import { resetUser } from "features/userSlice"
 import { resetObjectType } from "features/wizardObjectTypeSlice"
 import { resetFolder } from "features/wizardSubmissionFolderSlice"
-import { useAppSelector, useAppDispatch } from "hooks"
 import { pathWithLocale } from "utils"
 
 const useStyles = makeStyles(theme => ({
-  appBar: {
-    backgroundColor: theme.palette.common.white,
-
-    boxShadow: "0 0.1875rem 0.375rem rgba(0, 0, 0, 0.16)",
-  },
-  logo: {
-    height: "auto",
-    maxHeight: "2.5rem",
-    maxWidth: "100%",
-  },
-  brandLink: {
-    padding: theme.spacing(0.5, 0.75),
-  },
   link: {
     margin: theme.spacing(1, 1.5),
-    color: "inherit",
+    color: theme.palette.secondary.main,
   },
   languageSelector: {
     marginLeft: theme.spacing(1),
@@ -48,20 +37,33 @@ const useStyles = makeStyles(theme => ({
   linkButton: {
     margin: theme.spacing(1, 1.5),
   },
-  title: {
-    flexGrow: 1,
-    color: theme.palette.darkGrey,
-  },
+}))
+
+const NavBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: theme.palette.common.white,
+  boxShadow: "0 0.25em 0.25em 0 rgba(0, 0, 0,0.25)",
+}))
+
+const Logo = styled("img")(() => ({
+  width: "4em",
+  height: "2.5em",
+}))
+
+const ServiceTitle = styled(Typography)(({ theme }) => ({
+  flexGrow: 1,
+  color: theme.palette.secondary.main,
+  fontWeight: 700,
 }))
 
 type MenuItemProps = {
-  currentLocale: string
+  currentLocale: string,
 }
 
 const NavigationLinks = (props: MenuItemProps) => {
   const { currentLocale } = props
+
   const classes = useStyles()
-  const dispatch = useAppDispatch()
+  const dispatch = useDispatch()
 
   const resetWizard = () => {
     dispatch(resetObjectType())
@@ -73,7 +75,7 @@ const NavigationLinks = (props: MenuItemProps) => {
       <IconButton
         component={RouterLink}
         to={`/${currentLocale}/home`}
-        // className={classes.HomeIcon}
+        className={classes.HomeIcon}
         aria-label="go to frontpage"
         color="inherit"
         size="large"
@@ -114,10 +116,12 @@ const NavigationLinks = (props: MenuItemProps) => {
 
 const LanguageSelector = (props: MenuItemProps) => {
   const { currentLocale } = props
+
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
+
   const classes = useStyles()
-  const dispatch = useAppDispatch()
+  const dispatch = useDispatch()
 
   const navigate = useNavigate()
 
@@ -129,13 +133,14 @@ const LanguageSelector = (props: MenuItemProps) => {
     }
 
     dispatch(setLocale(locale))
+
     i18n.changeLanguage(locale).then(t => {
       t("key")
       handleClose()
     })
   }
 
-  const handleClick = (event: any) => {
+  const handleClick = event => {
     setAnchorEl(event.currentTarget)
   }
 
@@ -144,7 +149,7 @@ const LanguageSelector = (props: MenuItemProps) => {
   }
 
   return (
-    <React.Fragment>
+    <>
       <Button
         id="lang-selector"
         aria-controls="lang-menu"
@@ -167,36 +172,38 @@ const LanguageSelector = (props: MenuItemProps) => {
         <MenuItem onClick={() => changeLang("en")}>En</MenuItem>
         <MenuItem onClick={() => changeLang("fi")}>Fi</MenuItem>
       </Menu>
-    </React.Fragment>
+    </>
   )
 }
 
 const NavigationMenu = () => {
-  const location = useLocation()
-  const currentLocale = useAppSelector(state => state.locale) || Locale.defaultLocale
+  const classes = useStyles()
+
+  let location = useLocation()
+
+  const currentLocale = useSelector(state => state.locale) || Locale.defaultLocale
 
   return (
-    <nav>
+    <nav className={classes.nav}>
       {location.pathname !== "/" && <NavigationLinks currentLocale={currentLocale} />}
       <LanguageSelector currentLocale={currentLocale} />
     </nav>
   )
 }
 
-const Nav: React.FC = () => {
-  const classes = useStyles()
+const Nav = (): React$Element<typeof AppBar> => {
   return (
-    <AppBar className={classes.appBar}>
+    <NavBar>
       <Toolbar>
-        <Link to={pathWithLocale("home")} component={RouterLink} className={classes.brandLink}>
-          <img className={classes.logo} src={logo} alt="CSC" />
+        <Link to={pathWithLocale("home")} component={RouterLink} sx={{ m: "1.5vh 1vw 1.5vh 2.5vw" }}>
+          <Logo src={logo} alt="CSC_logo" />
         </Link>
-        <Typography variant="h6" noWrap className={classes.title}>
+        <ServiceTitle variant="h6" noWrap>
           Sensitive Data Services - SD Submit
-        </Typography>
+        </ServiceTitle>
         <NavigationMenu />
       </Toolbar>
-    </AppBar>
+    </NavBar>
   )
 }
 

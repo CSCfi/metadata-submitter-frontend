8ff5620002674ffa1f342b323b72aa95b505f7c9
Liisa Lado-Villar <47525503+lilachic@users.noreply.github.com>
2022-06-30 14:59:49 +0300

    resume resetdb

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'cypress'
+import setupNodeEvents from './cypress/plugins/index.js'
+//import { MongoClient } from "mongodb"
 
 export default defineConfig({
   env: {
@@ -13,10 +15,19 @@ export default defineConfig({
   videoCompression: false,
   defaultCommandTimeout: 10000,
   e2e: {
+    setupNodeEvents,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    // setupNodeEvents(on, config) {
+    //   const database = await MongoClient.connect("mongodb://admin:admin@localhost:27017")
+    //   on("task", {
+    //     resetDb() {
+    //       const db = database.db("default")
+    //       db.dropDatabase()
+    //       return null
+    //     },
+    //   })
+    // },
   },
+  chromeWebSecurity: false,
 })

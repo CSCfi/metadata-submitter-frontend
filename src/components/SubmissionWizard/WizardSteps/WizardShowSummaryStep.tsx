c298e94668c6c4fb76ef5a79712c70939f4c82dd
Mariia Rogina <roginama@csc.fi>
2024-09-03 10:53:46 +0300

    Remove old commented out code

--- src/components/SubmissionWizard/WizardSteps/WizardShowSummaryStep.tsx
+++ src/components/SubmissionWizard/WizardSteps/WizardShowSummaryStep.tsx
@@ -5,13 +5,6 @@ import { AppBar, Toolbar } from "@mui/material"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
-//import Dialog from "@mui/material/Dialog"
-//import DialogActions from "@mui/material/DialogActions"
-//import DialogContent from "@mui/material/DialogContent"
-//import DialogContentText from "@mui/material/DialogContentText"
-//import DialogTitle from "@mui/material/DialogTitle"
-// import Grid from "@mui/material/Grid"
-// import Link from "@mui/material/Link"
 import { styled } from "@mui/material/styles"
 import Typography from "@mui/material/Typography"
 import {
@@ -24,27 +17,15 @@ import {
 import { useTranslation } from "react-i18next"
 import { useNavigate } from "react-router-dom"
 
-//import WizardAlert from "../WizardComponents/WizardAlert"
 import WizardObjectStatusBadge from "../WizardComponents/WizardObjectStatusBadge"
 import WizardPagination from "../WizardComponents/WizardPagination"
-// import WizardDOIForm from "../WizardForms/WizardDOIForm"
 import editObjectHook from "../WizardHooks/WizardEditObjectHook"
 import WizardMapObjectsToStepHook from "../WizardHooks/WizardMapObjectsToStepsHook"
-// import saveDraftsAsTemplates from "../WizardHooks/WizardSaveTemplatesHook"
 
-// import { ResponseStatus } from "constants/responseStatus"
-// import { DisplayObjectTypes } from "constants/wizardObject"
-// import { resetAutocompleteField } from "features/autocompleteSlice"
-// import { resetFileTypes } from "features/fileTypesSlice"
-// import { setOpenedDoiForm } from "features/openedDoiFormSlice"
-// import { updateStatus } from "features/statusMessageSlice"
-// import { setCurrentObject, resetCurrentObject } from "features/wizardCurrentObjectSlice"
 import { resetObjectType } from "features/wizardObjectTypeSlice"
 import { updateStep } from "features/wizardStepObjectSlice"
-// import { publishSubmissionContent, resetSubmission } from "features/wizardSubmissionSlice"
 import { useAppSelector, useAppDispatch } from "hooks"
 import workflowAPIService from "services/workflowAPI"
-// import type { ObjectInsideSubmissionWithTags, Workflow } from "types"
 import type { Workflow } from "types"
 import { pathWithLocale } from "utils"
 
@@ -93,45 +74,15 @@ const SummaryTable = styled(DataGrid)(({ theme }) => ({
   },
 }))
 
-// const StepContainer = styled(Container)(({ theme }) => {
-//   const itemBorder = `1px solid ${theme.palette.secondary.lightest}`
-//   return {
-//     "& .MuiGrid-container": { border: itemBorder, borderBottom: 0 },
-//     "& .MuiGrid-container:last-child": { borderBottom: itemBorder },
-//     "& ul:first-of-type p:first-of-type": { paddingTop: "0 !important" },
-//   }
-// })
-
-// const SummaryItem = styled(Grid)(({ theme }) => ({
-//   margin: 0,
-//   padding: theme.spacing(1.5, 2),
-//   "& .MuiGrid-item": { padding: theme.spacing(0, 1), alignSelf: "center" },
-// }))
-
-// const DraftHelperGridItem = styled(Grid)(({ theme }) => ({
-//   flexGrow: "2 !important",
-//   color: theme.palette.secondary.main,
-// }))
-
-// const ButtonContainer = styled("div")(({ theme }) => ({
-//   marginTop: theme.spacing(3),
-//   display: "flex",
-//   width: "100%",
-//   justifyContent: "space-between",
-// }))
-
 ///**
 // * Show summary of objects added to submission
 // */
 const WizardShowSummaryStep: React.FC = () => {
   const submission = useAppSelector(state => state.submission)
   const workflowType = useAppSelector(state => state.workflowType)
-  //const openedDoiForm = useAppSelector(state => state.openedDoiForm)
-  //const projectId = useAppSelector(state => state.projectId)
   const objectTypesArray = useAppSelector(state => state.objectTypesArray)
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
-  //const [dialogOpen, setDialogOpen] = useState(false)
 
   const { t } = useTranslation()
 
@@ -156,73 +107,6 @@ const WizardShowSummaryStep: React.FC = () => {
     }
     getWorkflow()
   }, [workflowType])
-
-  // const DOIDialog = () => (
-  //   <Dialog
-  //     maxWidth="md"
-  //     fullWidth
-  //     open={openedDoiForm}
-  //     onClose={() => dispatch(setOpenedDoiForm(false))}
-  //     aria-labelledby="doi-dialog-title"
-  //     aria-describedby="doi-dialog-description"
-  //   >
-  //     <DialogTitle id="doi-dialog-title">DOI information</DialogTitle>
-  //     <DialogContent>
-  //       <DialogContentText id="doi-dialog-description" data-testid="doi-dialog-content">
-  //         Please fill in and save the information if you would like your submission to have DOI.
-  //       </DialogContentText>
-  //       <WizardDOIForm formId="doi-form" />
-  //     </DialogContent>
-  //     <DialogActions>
-  //       <Button variant="contained" onClick={handleCancelDoiDialog} color="secondary">
-  //         Cancel
-  //       </Button>
-  //       <Button variant="contained" color="primary" type="submit" form="doi-form">
-  //         Save DOI info
-  //       </Button>
-  //     </DialogActions>
-  //   </Dialog>
-  // )
-  // const handleCancelDoiDialog = () => {
-  //   dispatch(setOpenedDoiForm(false))
-  //   dispatch(resetAutocompleteField())
-  //   dispatch(resetCurrentObject())
-  // }
-  // const handleOpenDoiDialog = () => {
-  //   dispatch(setOpenedDoiForm(true))
-  //   dispatch(setCurrentObject(submission.doiInfo))
-  // }
-  // const handlePublishDialog = async (
-  //   alertWizard: boolean,
-  //   formData?: Array<ObjectInsideSubmissionWithTags>
-  // ) => {
-  //   const resetDispatch = () => {
-  //     navigate(pathWithLocale("home"))
-  //     dispatch(resetObjectType())
-  //     dispatch(resetSubmission())
-  //   }
-  //   if (alertWizard) {
-  //     if (formData && formData?.length > 0) {
-  //       await saveDraftsAsTemplates(projectId, formData, dispatch)
-  //     }
-  //     // Publish the submission
-  //     dispatch(publishSubmissionContent(submission))
-  //       .then(() => resetDispatch())
-  //       .catch(error => {
-  //         dispatch(
-  //           updateStatus({
-  //             status: ResponseStatus.error,
-  //             response: error,
-  //             helperText: `Couldn't publish submission with id ${submission.submissionId}`,
-  //           })
-  //         )
-  //       })
-  //     dispatch(resetFileTypes())
-  //   } else {
-  //     setDialogOpen(false)
-  //   }
-  //   setDialogOpen(false)
-  // }
 
   useEffect(() => {
     WizardMapObjectsToStepHook(submission, objectTypesArray, currentWorkflow, t)
@@ -353,11 +237,7 @@ const WizardShowSummaryStep: React.FC = () => {
         </Toolbar>
       </SummaryBar>
       <Container>
-        <Typography
-          component="h1"
-          variant="h4"
-          color="secondary"
-        >
+        <Typography component="h1" variant="h4" color="secondary">
           {t("summary")}
         </Typography>
       </Container>
@@ -417,27 +297,6 @@ const WizardShowSummaryStep: React.FC = () => {
           </Container>
         )
       })}
-
-      {/* <ButtonContainer>
-        <Button variant="contained" color="secondary" onClick={handleOpenDoiDialog}>
-          Add DOI information (optional)
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => setDialogOpen(true)}
-          data-testid="summary-publish"
-        >
-          Publish
-        </Button>
-      </ButtonContainer>
-      {openedDoiForm && <DOIDialog />}
-      {dialogOpen && (
-        <WizardAlert
-          onAlert={handlePublishDialog}
-          parentLocation="footer"
-          alertType={"publish"}
-        ></WizardAlert>
-      )} */}
     </>
   )
 }

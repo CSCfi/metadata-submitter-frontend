360fc0ddd23f73c4dd57c8d95920162d8e1f916b
Liisa Lado-Villar <145-lilado@users.noreply.gitlab.ci.csc.fi>
2025-08-21 14:30:10 +0300

    improve e2e appSDSX test

--- playwright/tests/appSDSX.spec.ts
+++ playwright/tests/appSDSX.spec.ts
@@ -7,10 +7,13 @@ test.describe("Basic SDSX flow", () => {
   test.beforeEach(async ({ resetDB }) => {
     await resetDB
   })
-  test("completes SDSX steps and publishes a new submissio", async ({
+  test("completes SDSX steps and publishes a new submission", async ({
     page,
     login,
     newSubmission,
+    formActions,
+    clickAccordionPanel,
+    clickAddObject,
   }) => {
     test.slow()
 
@@ -31,12 +34,8 @@ test.describe("Basic SDSX flow", () => {
     ).toBeVisible()
     await page.getByRole("combobox", { name: "Select organization" }).click()
     await page.getByTestId("CSC").click()
-    await page.getByRole("radio", { name: "another_org workflow" }).check()
-    await page
-      .locator("div")
-      .filter({ hasText: /^Save$/ })
-      .first()
-      .click()
+    await page.getByTestId("1").getByRole("radio").check()
+    await formActions("form-ready")
 
     /*
      * 3rd step, Datafolder
@@ -45,26 +44,16 @@ test.describe("Basic SDSX flow", () => {
     await page.getByRole("button", { name: "Datafolder" }).click()
     await page.getByTestId("View file").click()
     await page.getByRole("radio", { name: "folderA" }).check()
-    expect(page.getByTestId("link-datafolder")).toBeEnabled()
+    await expect(page.getByTestId("link-datafolder")).toBeEnabled()
     await page.getByTestId("link-datafolder").click()
 
     /*
-     * 4th step, Describe
-     */
-    await page.getByRole("button", { name: "Describe" }).click()
-    await page.getByTestId("Add dataset").click()
-    await page.getByTestId("title").click()
-    await page.getByTestId("title").fill("Dataset title")
-    await page.getByTestId("description").click()
-    await page.getByTestId("description").fill("Dataset description")
-    await page.getByTestId("form-ready").click()
-
-    /*
-     * 5th step, Identifier and publish
+     * 4th step, Identifier and publish
      */
 
-    await page.getByRole("button", { name: "Identifier and publish" }).click()
-    await page.getByTestId("Add datacite").click()
+    // DOI
+    await clickAccordionPanel("Identifier and publish")
+    await clickAddObject("datacite")
     await page
       .locator("div")
       .filter({ hasText: /^Creators\*Add new item$/ })
@@ -82,13 +71,14 @@ test.describe("Basic SDSX flow", () => {
     await page.getByTestId("creators.0.affiliation.0.name-inputField").click()
     await page.getByTestId("creators.0.affiliation.0.name-inputField").fill("a")
     await page.getByRole("option", { name: "A&A Biotechnology (Poland)" }).click()
-    await page
-      .locator("div")
-      .filter({ hasText: /^Subjects\*Add new item$/ })
-      .getByRole("button")
-      .click()
-    await page.getByTestId("subjects.0.subject").click()
-    await page.getByTestId("subjects.0.subject").selectOption("1 - Natural sciences")
+    await page.getByTestId("keywords").fill("keyword")
+    await formActions("form-datacite")
+
+    // Summary
+    await page.getByTestId("View Summary").click()
+    await expect(
+      page.getByTestId("summary-step-1").getByRole("heading", { name: "1. Submission details" })
+    ).toBeVisible()
   })
 })
 

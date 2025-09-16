da7a6eb3f7deab2efb0bd3a0af5f7128ba00fda4
Monika Radaviciute <mradavic@csc.fi>
2025-09-16 17:23:58 +0300

    Change folder icon to bucket icon

--- src/components/SubmissionWizard/WizardComponents/WizardDataBucketTable.tsx
+++ src/components/SubmissionWizard/WizardComponents/WizardDataBucketTable.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react"
 
-import FolderIcon from "@mui/icons-material/Folder"
 import Box from "@mui/material/Box"
 import Radio from "@mui/material/Radio"
+import SvgIcon from "@mui/material/SvgIcon"
 import Typography from "@mui/material/Typography"
 import { GridColDef } from "@mui/x-data-grid"
 import type { GridSortDirection } from "@mui/x-data-grid"
@@ -52,12 +52,15 @@ const WizardDataBucketTable: React.FC<DataBucketTableProps> = props => {
                 inputProps={{ "aria-label": params.row.name }}
               />
             )}
-            <FolderIcon
+            {/* Use mdiPail path: MUI icons lack a corresponding icon */}
+            <SvgIcon
               color="primary"
               fontSize="medium"
               sx={{ ml: "1rem", mr: "0.5rem" }}
               onClick={() => handleFilesView(params.row.name)}
-            />
+            >
+              <path d="M11.5 7.63C11.97 7.35 12.58 7.5 12.86 8C13.14 8.47 12.97 9.09 12.5 9.36L4.27 14.11C3.79 14.39 3.18 14.23 2.9 13.75C2.62 13.27 2.79 12.66 3.27 12.38L11.5 7.63M7 21L5.79 14.97L13.21 10.69C14 10.26 14.5 9.44 14.5 8.5C14.5 7.12 13.38 6 12 6C11.53 6 11.09 6.13 10.71 6.36L4.76 9.79L4 6H3V4H21V6H20L17 21H7Z" />
+            </SvgIcon>
             <Typography component="span" onClick={() => handleFilesView(params.row.name)}>
               {upperFirst(params.row.name)}
             </Typography>
